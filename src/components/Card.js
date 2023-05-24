733570a104e81fed963ebaf6a67c49f17b13989e
lamtaoui mohamed <el-hafid-@hotmail.fr>
2023-05-24 10:53:51 +0200

    commit suite fichier Card renommer

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+function Card(){
+    return (
+        <section>
+            <article>
+                <h1>Mes images</h1>
+            </article>
+        </section>
+    )
+}
+
+export default Card
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,12 @@
-import "../style/home.css"
-import plage from '../image/image-home.png'
+import Banner from './Banner'
+import Card from './Card'
 
 function Home(){
     return(
-       <body>
-            <div className="title-home" >
-                <img src={plage} alt="la mer avec des roches" height="223px" width="100%"></img>
-                <h1>Chez vous, partout et ailleurs</h1>
-            </div>
-       </body>  
+        <div>
+            <Banner/>
+            <Card/>
+        </div>
     )
 }
 export default Home
