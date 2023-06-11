c15a02ec8304f6b252a19d6a2bea587fe8456292
lamtaoui mohamed <el-hafid-@hotmail.fr>
2023-06-11 16:44:33 +0200

    tentative useNavigate with useEffect

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Header/>
       <Routes>
           <Route path="/" element={<Home />}/>
-          <Route path={"/Fiche/:idFiche"}  element={<Fiche />} /> 
+          <Route path={"/Fiche/:idFiche"} element={<Fiche/>} />
           <Route path="/Propos" element={<About />} />
           <Route path="/*" element={<Error />} />
       </Routes>
--- src/components/Fiche.js
+++ src/components/Fiche.js
@@ -1,81 +1,80 @@
+import { useParams } from "react-router-dom"
 import Slide from './Slide'
 import '../style/fiche.scss'
 import StarOrange from '../assets/image/star-orange.png'
 import StarGrey from '../assets/image/star-grey.png'
-import {useParams, useNavigate, useLocation} from "react-router-dom"
 import Data from '../data/annonce.json'
 import Collapse from './Collapse'
-
-
+import { useNavigate } from "react-router-dom"
+import { useEffect } from "react"
 
 
 function Fiche() {
-        const mylocationpath = useLocation().pathname
-        console.log(mylocationpath)
-        const { idFiche } = useParams();
-        const dataFind = Data.find(item => item.id === idFiche);
-        const navigate = useNavigate();
-        let pictures = dataFind.pictures;
-        let title = dataFind.title;
-        let location = dataFind.location;
-        let buttons = dataFind.tags;
-        let profilName = dataFind.host.name;
-        let profilPicture = dataFind.host.picture;
-        let description = dataFind.description;
-        let equipment = dataFind.equipments;
-                
-        //essaye de rappeler mylocationpath
-        if (!mylocationpath) {               
+
+                const navigate = useNavigate();//ok
+                const { idFiche } = useParams();//ok
+                const dataFind = Data.find(item => item.id === idFiche);
+                let pictures = dataFind.pictures;
+                let title = dataFind.title;
+                let location = dataFind.location;
+                let buttons = dataFind.tags;
+                let profilName = dataFind.host.name;
+                let profilPicture = dataFind.host.picture;
+                let description = dataFind.description;
+                let equipment = dataFind.equipments;
+        
+        
+                 useEffect(() => {
+                        const secondUrlFiche = window.location.pathname;
+                        console.log(secondUrlFiche)
+                         if ((secondUrlFiche) === ('/Fiche/' + { idFiche })) {
+                                 navigate('/*');
+                         } 
+                        });
+                                 return (
+                                         <div className='main-contain'>
                         
-                        navigate('/*')
-                        console.log("bidon")
-                       
-        } else {
-                return (
-                  
-                <div className='main-contain'>
+                                                 <div>
+                                                         <Slide arrayOfSlide={pictures} />
+                                                 </div>
+                                                 <div className='flex-title-profil'>
+                                                         <div>
+                                                                 <h1>{title}</h1>
+                                                                 <p>{location}</p>
+                                                
+                                                                 {buttons.map((item, index) => (<button key={index}>{item}</button>))}
+                                                
+                                                         </div>
+                                                         <div className='flex-profil'>
+                                                                 <div className='flex-profil_withoutstars'>
+                                                                         <p>{profilName}</p>
+                                                                         <img src={profilPicture} alt='profil-fiche'></img>
+                                                                 </div>
+                                                
+                                                                 <div>
+                                                                         <img src={StarOrange} alt='étoile orange'></img>
+                                                                         <img src={StarOrange} alt='étoile orange'></img>
+                                                                         <img src={StarOrange} alt='étoile orange'></img>
+                                                                         <img src={StarGrey} alt='étoile grise'></img>
+                                                                         <img src={StarGrey} alt='étoile grise'></img>
+                                                                 </div>
+                                                         </div>
+                                                 </div>
+                                                 <div>
+                                                         <Collapse description={description} equipment={equipment} />
+                                                 </div>
                         
-                        <div>
-                                <Slide arrayOfSlide={pictures} />
-                        </div>
-                        <div className='flex-title-profil'>
-                                <div>
-                                        <h1>{title}</h1>
-                                        <p>{location}</p>
-                                                
-                                        {buttons.map((item, index) => (<button key={index}>{item}</button>))}
-                                                
-                                </div>
-                                <div className='flex-profil'>
-                                        <div className='flex-profil_withoutstars'>
-                                                <p>{profilName}</p>
-                                                <img src={profilPicture} alt='profil-fiche'></img>
-                                        </div>
-                                                
-                                        <div>
-                                                <img src={StarOrange} alt='étoile orange'></img>
-                                                <img src={StarOrange} alt='étoile orange'></img>
-                                                <img src={StarOrange} alt='étoile orange'></img>
-                                                <img src={StarGrey} alt='étoile grise'></img>
-                                                <img src={StarGrey} alt='étoile grise'></img>
-                                        </div>
-                                </div>
-                        </div>
-                        <div>
-                                <Collapse description={description} equipment={equipment} />
-                        </div>
-                       
-                </div>       
-                )
+                                         </div>
+                                 )
+                             
+        
+        
+        
         }
         
-                        
-                
-                
-        }
-        export default Fiche;
-  
-  
+        
+export default Fiche;       
+
 
 
 
