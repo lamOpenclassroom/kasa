899634a5e564f8b21703314954053c47d32775f0
lamtaoui mohamed <el-hafid-@hotmail.fr>
2023-06-16 19:56:18 +0200

    sauvegarde responsive header + card

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ function Banner({ montain, beach, titleHome }) {
     
         return (
             <div className="title-home" >
-                <img src={beach||montain} alt="la mer avec des roches" height="223px" width="100%"></img>  
+                <img src={beach||montain} alt="la mer avec des roches et la montaggne" height="223px" width="100%"></img>  
                 {titleHome}
             </div>
         )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import Banner from './Banner'
 import beach from '../assets/image/image-home.png'
 import Card from './Card'
 import Data from '../data/annonce.json'
-import '../style/home.scss'
 
 function Home() {
    const titleHome = <h1>Chez vous, partout et ailleurs</h1>
